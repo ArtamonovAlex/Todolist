68e3942ea37f9c7de7b6156df1af64179e94118b
ArtamonovAlex <artamonovalex18@gmail.com>
2018-05-31 17:59:29 +0300

    Added disconnect exception

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,59 +13,22 @@ namespace Client
                 Console.WriteLine("Wrong arguments");
                 return;
             }
-            var client = new Editor.TodoEditorClient("NetTcpBinding_ITodoEditor");
-            if (!client.InitializeList(args[0]))
+            try
             {
-                Console.WriteLine("Can't access file. Abort process");
-                return;
-            }
-            bool isWorking = true;
-            while (isWorking)
-            {
-                switch (TodoIO.GetMenuOption())
+                var client = new Editor.TodoEditorClient("NetTcpBinding_ITodoEditor");
+                if (!client.InitializeList(args[0]))
                 {
-                    case 1:
-                        {
-                            client.AddTask(JsonConverter.ToJson(TodoIO.AddTask()));
-                            if (!client.SaveMain(args[0]))
+                    Console.WriteLine("Can't access file. Abort process");
+                    return;
+                }
+                bool isWorking = true;
+                while (isWorking)
+                {
+                    switch (TodoIO.GetMenuOption())
+                    {
+                        case 1:
                             {
-                                Console.WriteLine("Can't save changes. They won't be avaliable to other clients.");
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("Task added!");
-                            }
-                            break;
-                        }
-                    case 2:
-                        {
-                            if (!client.InitializeList(args[0]))
-                            {
-                                Console.WriteLine("Can't access file. Abort process");
-                                break;
-                            }
-                            TodoList list = JsonConverter.GetObject<TodoList>(client.GetList());
-                            TodoIO.PrintAll(list);
-                            Console.WriteLine("All tasks printed!");
-                            break;
-                        }
-                    case 3:
-                        {
-                            if (!client.InitializeList(args[0]))
-                            {
-                                Console.WriteLine("Can't access file. Abort process");
-                                break;
-                            }
-                            List<Task> tasks = JsonConverter.GetObject<List<Task>>(client.FindTasks(TodoIO.GetTags()));
-                            if (TodoIO.FoundedTasks(tasks))
-                            {
-                                TodoList todo = JsonConverter.GetObject<TodoList>(client.GetList());
-                                foreach (Task task in tasks)
-                                {
-                                    todo.Tasks.Remove(task);
-                                }
-                                client.DeleteTasks(JsonConverter.ToJson(todo));
+                                client.AddTask(JsonConverter.ToJson(TodoIO.AddTask()));
                                 if (!client.SaveMain(args[0]))
                                 {
                                     Console.WriteLine("Can't save changes. They won't be avaliable to other clients.");
@@ -73,58 +36,102 @@ namespace Client
                                 }
                                 else
                                 {
-                                    Console.WriteLine("Deleted!");
+                                    Console.WriteLine("Task added!");
                                 }
+                                break;
                             }
-                            break;
-                        }
-                    case 4:
-                        {
-                            string path = TodoIO.ReadPath();
-                            if (path != "cancel")
+                        case 2:
                             {
-                                if (!client.Load(path))
+                                if (!client.InitializeList(args[0]))
                                 {
                                     Console.WriteLine("Can't access file. Abort process");
                                     break;
                                 }
-                                Console.WriteLine("Loaded!");
-                            }
-                            break;
-                        }
-                    case 5:
-                        {
-                            if (!client.InitializeList(args[0]))
-                            {
-                                Console.WriteLine("Can't access file. Abort process");
+                                TodoList list = JsonConverter.GetObject<TodoList>(client.GetList());
+                                TodoIO.PrintAll(list);
+                                Console.WriteLine("All tasks printed!");
                                 break;
                             }
-                            string path = TodoIO.ReadPath();
-                            if (path != "cancel")
+                        case 3:
                             {
-                                if (!client.SaveMain(args[0]))
+                                if (!client.InitializeList(args[0]))
                                 {
-                                    Console.WriteLine("Can't save changes. They won't be avaliable to other clients.");
+                                    Console.WriteLine("Can't access file. Abort process");
                                     break;
                                 }
-                                Console.WriteLine("Saved!");
+                                List<Task> tasks = JsonConverter.GetObject<List<Task>>(client.FindTasks(TodoIO.GetTags()));
+                                if (TodoIO.FoundedTasks(tasks))
+                                {
+                                    TodoList todo = JsonConverter.GetObject<TodoList>(client.GetList());
+                                    foreach (Task task in tasks)
+                                    {
+                                        todo.Tasks.Remove(task);
+                                    }
+                                    client.DeleteTasks(JsonConverter.ToJson(todo));
+                                    if (!client.SaveMain(args[0]))
+                                    {
+                                        Console.WriteLine("Can't save changes. They won't be avaliable to other clients.");
+
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Deleted!");
+                                    }
+                                }
+                                break;
                             }
-                            break;
-                        }
-                    case 6:
-                        {
-                            client.SaveMain(args[0]);
-                            Console.WriteLine("Goodbye!");
-                            Console.ReadLine();
-                            isWorking = false;
-                            break;
-                        }
-                    default:
-                        {
-                            Console.WriteLine("Wrong imput, choose the correct option");
-                            break;
-                        }
+                        case 4:
+                            {
+                                string path = TodoIO.ReadPath();
+                                if (path != "cancel")
+                                {
+                                    if (!client.Load(path))
+                                    {
+                                        Console.WriteLine("Can't access file. Abort process");
+                                        break;
+                                    }
+                                    Console.WriteLine("Loaded!");
+                                }
+                                break;
+                            }
+                        case 5:
+                            {
+                                if (!client.InitializeList(args[0]))
+                                {
+                                    Console.WriteLine("Can't access file. Abort process");
+                                    break;
+                                }
+                                string path = TodoIO.ReadPath();
+                                if (path != "cancel")
+                                {
+                                    if (!client.SaveMain(args[0]))
+                                    {
+                                        Console.WriteLine("Can't save changes. They won't be avaliable to other clients.");
+                                        break;
+                                    }
+                                    Console.WriteLine("Saved!");
+                                }
+                                break;
+                            }
+                        case 6:
+                            {
+                                client.SaveMain(args[0]);
+                                Console.WriteLine("Goodbye!");
+                                Console.ReadLine();
+                                isWorking = false;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Wrong imput, choose the correct option");
+                                break;
+                            }
+                    }
                 }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Хост разорвал соединение");
             }
         }
     }
